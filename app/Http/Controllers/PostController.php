1c04cda5cc369b9b057c4b305058d17d596b4cdd
sqtatdev <vjatseslac.rovvlin@tptlive.ee>
2025-12-12 11:42:01 +0200

    Add default user seeding

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -30,7 +31,10 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        dd($request->all());
+        $post = new Post($request->validated());
+        $post->user()->associate(Auth::user());
+        $post->save();
+        return redirect()->route('posts.index');
     }
 
     /**
